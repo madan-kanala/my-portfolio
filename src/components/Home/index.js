ccece0fcb126af991d02ffe741813d21a0b825f8
madak-kanala <mdnkanala.dev@gmail.com>
2022-12-19 20:55:01 -0500

    update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,15 @@
 import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
 import Loader from 'react-loaders'
+import { Link } from 'react-router-dom'
+// import LogoTitle from '../../assets/images/logo-m.png'
 import AnimatedLetters from '../AnimatedLetters'
-import LogoTitle from '../../assets/images/logo-s.png'
-import Logo from './Logo'
 import './index.scss'
+import Logo from './Logo'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
 
-  const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
+  const nameArray = ['M', 'a', 'd', 'a', 'n', '', 'K', 'a', 'n', 'a', 'l', 'a']
   const jobArray = [
     'w',
     'e',
@@ -42,11 +42,11 @@ const Home = () => {
             <span className={`${letterClass} _12`}>i,</span>
             <br />
             <span className={`${letterClass} _13`}>I</span>
-            <span className={`${letterClass} _14`}>'m</span>
-            <img
+            <span className={`${letterClass} _14`}>'m &nbsp;</span>
+            {/* <img
               src={LogoTitle}
               alt="JavaScript Developer Name, Web Developer Name"
-            />
+            /> */}
             <AnimatedLetters
               letterClass={letterClass}
               strArray={nameArray}
@@ -59,7 +59,9 @@ const Home = () => {
               idx={22}
             />
           </h1>
-          <h2>Front End Developer / JavaScript Expert / Youtuber</h2>
+          <h2 style={{ fontSize: 20 }}>
+            Front End Developer / JavaScript Expert / Youtuber
+          </h2>
           <Link to="/contact" className="flat-button">
             CONTACT ME
           </Link>
--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,22 @@
-
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { getStorage } from 'firebase/storage';
-import { getFirestore } from 'firebase/firestore/lite';
+import { initializeApp } from 'firebase/app'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore/lite'
+import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
-};
+  apiKey: 'AIzaSyAoI7SpnAI5kIB44cl0twiMU_wahFE8PAg',
+  authDomain: 'porfolio-11cd9.firebaseapp.com',
+  projectId: 'porfolio-11cd9',
+  storageBucket: 'porfolio-11cd9.appspot.com',
+  messagingSenderId: '114284663956',
+  appId: '1:114284663956:web:79100d8172520fb6769081',
+  measurementId: 'G-47EF6KL903',
+}
 
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth()
+const provider = new GoogleAuthProvider()
+export const db = getFirestore(app)
+export const storage = getStorage(app)
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth();
-const provider = new GoogleAuthProvider();
-export const db = getFirestore(app);
-export const storage = getStorage(app);
-
-
-export const signInWithGoogle = () => signInWithPopup(auth, provider);
+export const signInWithGoogle = () => signInWithPopup(auth, provider)
